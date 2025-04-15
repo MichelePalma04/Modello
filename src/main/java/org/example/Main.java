11b1e105ffa264f178579f9a308b868154bbb8b8
MichelePalma04 <michelepalma04@gmail.com>
2025-04-15 17:35:33 +0200

    prova 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ciao progetto");
+        System.out.println("Ciao negretto");
     }
 }
